18f19c64ef77ce70274e4a6fda0d5a7d5490df34
CReMV <chrisvasilas1993@yahoo.gr>
2016-10-28 16:09:02 +0300

    Revert "Έτοιμο"
    This reverts commit 91e406531ac6470ef5cb3162635639b2aa5bdaf2.

--- WindowsApplication3/WindowsApplication3/Form1.vb
+++ WindowsApplication3/WindowsApplication3/Form1.vb
@@ -2,18 +2,6 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Form2.Show()
         Form2.TextBox1.Text = TextBox1.Text
-        Form2.TextBox2.Text = TextBox2.Text
-        Form2.TextBox3.Text = TextBox33.Text
-        Form2.TextBox4.Text = TextBox34.Text
-        Form2.TextBox5.Text = TextBox35.Text
-        Form2.TextBox6.Text = TextBox36.Text
-        Form2.TextBox7.Text = TextBox37.Text
-        Form2.TextBox38.Text = TextBox38.Text
-        Form2.TextBox39.Text = TextBox39.Text
-        Form2.TextBox40.Text = TextBox40.Text
-        Form2.TextBox41.Text = TextBox41.Text
-        Form2.TextBox42.Text = TextBox42.Text
-        Form2.TextBox44.Text = TextBox44.Text
 
     End Sub
 
