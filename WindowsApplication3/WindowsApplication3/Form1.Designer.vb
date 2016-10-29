9607bd579c600a47982da5f0f7aef8fe3a5ecbfa
Alex <alexiskaselouris@gmail.com>
2016-10-29 03:16:42 +0300

    FINAL

--- WindowsApplication3/WindowsApplication3/Form1.Designer.vb
+++ WindowsApplication3/WindowsApplication3/Form1.Designer.vb
@@ -598,7 +598,7 @@ Partial Class Form1
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.TextBox1)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Scoreboard Input"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
--- WindowsApplication3/WindowsApplication3/Form2.Designer.vb
+++ WindowsApplication3/WindowsApplication3/Form2.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form2
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class Form2
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
@@ -66,6 +66,8 @@ Partial Class Form2
         Me.TextBox42 = New System.Windows.Forms.TextBox()
         Me.TextBox43 = New System.Windows.Forms.TextBox()
         Me.TextBox44 = New System.Windows.Forms.TextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'TextBox1
@@ -80,9 +82,10 @@ Partial Class Form2
         '
         'TextBox2
         '
+        Me.TextBox2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.TextBox2.BackColor = System.Drawing.Color.AliceBlue
         Me.TextBox2.Font = New System.Drawing.Font("Verdana", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox2.Location = New System.Drawing.Point(453, 31)
+        Me.TextBox2.Location = New System.Drawing.Point(655, 31)
         Me.TextBox2.Multiline = True
         Me.TextBox2.Name = "TextBox2"
         Me.TextBox2.Size = New System.Drawing.Size(400, 60)
@@ -276,7 +279,8 @@ Partial Class Form2
         '
         'TextBox23
         '
-        Me.TextBox23.Location = New System.Drawing.Point(453, 97)
+        Me.TextBox23.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox23.Location = New System.Drawing.Point(655, 97)
         Me.TextBox23.Multiline = True
         Me.TextBox23.Name = "TextBox23"
         Me.TextBox23.Size = New System.Drawing.Size(60, 40)
@@ -285,7 +289,8 @@ Partial Class Form2
         '
         'TextBox24
         '
-        Me.TextBox24.Location = New System.Drawing.Point(453, 144)
+        Me.TextBox24.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox24.Location = New System.Drawing.Point(655, 144)
         Me.TextBox24.Multiline = True
         Me.TextBox24.Name = "TextBox24"
         Me.TextBox24.Size = New System.Drawing.Size(60, 38)
@@ -294,7 +299,8 @@ Partial Class Form2
         '
         'TextBox25
         '
-        Me.TextBox25.Location = New System.Drawing.Point(453, 188)
+        Me.TextBox25.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox25.Location = New System.Drawing.Point(655, 188)
         Me.TextBox25.Multiline = True
         Me.TextBox25.Name = "TextBox25"
         Me.TextBox25.Size = New System.Drawing.Size(60, 39)
@@ -303,7 +309,8 @@ Partial Class Form2
         '
         'TextBox26
         '
-        Me.TextBox26.Location = New System.Drawing.Point(453, 233)
+        Me.TextBox26.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox26.Location = New System.Drawing.Point(655, 233)
         Me.TextBox26.Multiline = True
         Me.TextBox26.Name = "TextBox26"
         Me.TextBox26.Size = New System.Drawing.Size(60, 39)
@@ -312,7 +319,8 @@ Partial Class Form2
         '
         'TextBox27
         '
-        Me.TextBox27.Location = New System.Drawing.Point(453, 278)
+        Me.TextBox27.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox27.Location = New System.Drawing.Point(655, 278)
         Me.TextBox27.Multiline = True
         Me.TextBox27.Name = "TextBox27"
         Me.TextBox27.Size = New System.Drawing.Size(60, 39)
@@ -321,7 +329,8 @@ Partial Class Form2
         '
         'TextBox28
         '
-        Me.TextBox28.Location = New System.Drawing.Point(519, 97)
+        Me.TextBox28.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox28.Location = New System.Drawing.Point(721, 97)
         Me.TextBox28.Multiline = True
         Me.TextBox28.Name = "TextBox28"
         Me.TextBox28.Size = New System.Drawing.Size(60, 40)
@@ -330,7 +339,8 @@ Partial Class Form2
         '
         'TextBox29
         '
-        Me.TextBox29.Location = New System.Drawing.Point(519, 144)
+        Me.TextBox29.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox29.Location = New System.Drawing.Point(721, 144)
         Me.TextBox29.Multiline = True
         Me.TextBox29.Name = "TextBox29"
         Me.TextBox29.Size = New System.Drawing.Size(60, 38)
@@ -339,7 +349,8 @@ Partial Class Form2
         '
         'TextBox30
         '
-        Me.TextBox30.Location = New System.Drawing.Point(519, 188)
+        Me.TextBox30.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox30.Location = New System.Drawing.Point(721, 188)
         Me.TextBox30.Multiline = True
         Me.TextBox30.Name = "TextBox30"
         Me.TextBox30.Size = New System.Drawing.Size(60, 39)
@@ -348,7 +359,8 @@ Partial Class Form2
         '
         'TextBox31
         '
-        Me.TextBox31.Location = New System.Drawing.Point(519, 233)
+        Me.TextBox31.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox31.Location = New System.Drawing.Point(721, 233)
         Me.TextBox31.Multiline = True
         Me.TextBox31.Name = "TextBox31"
         Me.TextBox31.Size = New System.Drawing.Size(60, 39)
@@ -357,7 +369,8 @@ Partial Class Form2
         '
         'TextBox32
         '
-        Me.TextBox32.Location = New System.Drawing.Point(519, 278)
+        Me.TextBox32.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox32.Location = New System.Drawing.Point(721, 278)
         Me.TextBox32.Multiline = True
         Me.TextBox32.Name = "TextBox32"
         Me.TextBox32.Size = New System.Drawing.Size(60, 39)
@@ -366,7 +379,8 @@ Partial Class Form2
         '
         'TextBox33
         '
-        Me.TextBox33.Location = New System.Drawing.Point(585, 97)
+        Me.TextBox33.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox33.Location = New System.Drawing.Point(787, 97)
         Me.TextBox33.Multiline = True
         Me.TextBox33.Name = "TextBox33"
         Me.TextBox33.Size = New System.Drawing.Size(60, 40)
@@ -375,7 +389,8 @@ Partial Class Form2
         '
         'TextBox34
         '
-        Me.TextBox34.Location = New System.Drawing.Point(585, 144)
+        Me.TextBox34.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox34.Location = New System.Drawing.Point(787, 144)
         Me.TextBox34.Multiline = True
         Me.TextBox34.Name = "TextBox34"
         Me.TextBox34.Size = New System.Drawing.Size(60, 38)
@@ -384,7 +399,8 @@ Partial Class Form2
         '
         'TextBox35
         '
-        Me.TextBox35.Location = New System.Drawing.Point(585, 188)
+        Me.TextBox35.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox35.Location = New System.Drawing.Point(787, 188)
         Me.TextBox35.Multiline = True
         Me.TextBox35.Name = "TextBox35"
         Me.TextBox35.Size = New System.Drawing.Size(60, 39)
@@ -393,7 +409,8 @@ Partial Class Form2
         '
         'TextBox36
         '
-        Me.TextBox36.Location = New System.Drawing.Point(585, 233)
+        Me.TextBox36.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox36.Location = New System.Drawing.Point(787, 233)
         Me.TextBox36.Multiline = True
         Me.TextBox36.Name = "TextBox36"
         Me.TextBox36.Size = New System.Drawing.Size(60, 39)
@@ -402,7 +419,8 @@ Partial Class Form2
         '
         'TextBox37
         '
-        Me.TextBox37.Location = New System.Drawing.Point(585, 278)
+        Me.TextBox37.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox37.Location = New System.Drawing.Point(787, 278)
         Me.TextBox37.Multiline = True
         Me.TextBox37.Name = "TextBox37"
         Me.TextBox37.Size = New System.Drawing.Size(60, 39)
@@ -411,9 +429,10 @@ Partial Class Form2
         '
         'TextBox38
         '
+        Me.TextBox38.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.TextBox38.BackColor = System.Drawing.Color.AliceBlue
         Me.TextBox38.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox38.Location = New System.Drawing.Point(649, 97)
+        Me.TextBox38.Location = New System.Drawing.Point(851, 97)
         Me.TextBox38.Multiline = True
         Me.TextBox38.Name = "TextBox38"
         Me.TextBox38.Size = New System.Drawing.Size(204, 40)
@@ -422,9 +441,10 @@ Partial Class Form2
         '
         'TextBox39
         '
+        Me.TextBox39.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.TextBox39.BackColor = System.Drawing.Color.AliceBlue
         Me.TextBox39.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox39.Location = New System.Drawing.Point(649, 144)
+        Me.TextBox39.Location = New System.Drawing.Point(851, 144)
         Me.TextBox39.Multiline = True
         Me.TextBox39.Name = "TextBox39"
         Me.TextBox39.Size = New System.Drawing.Size(204, 38)
@@ -433,9 +453,10 @@ Partial Class Form2
         '
         'TextBox40
         '
+        Me.TextBox40.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.TextBox40.BackColor = System.Drawing.Color.AliceBlue
         Me.TextBox40.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox40.Location = New System.Drawing.Point(649, 188)
+        Me.TextBox40.Location = New System.Drawing.Point(851, 188)
         Me.TextBox40.Multiline = True
         Me.TextBox40.Name = "TextBox40"
         Me.TextBox40.Size = New System.Drawing.Size(204, 39)
@@ -444,9 +465,10 @@ Partial Class Form2
         '
         'TextBox41
         '
+        Me.TextBox41.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.TextBox41.BackColor = System.Drawing.Color.AliceBlue
         Me.TextBox41.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox41.Location = New System.Drawing.Point(649, 233)
+        Me.TextBox41.Location = New System.Drawing.Point(851, 233)
         Me.TextBox41.Multiline = True
         Me.TextBox41.Name = "TextBox41"
         Me.TextBox41.Size = New System.Drawing.Size(204, 39)
@@ -455,9 +477,10 @@ Partial Class Form2
         '
         'TextBox42
         '
+        Me.TextBox42.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.TextBox42.BackColor = System.Drawing.Color.AliceBlue
         Me.TextBox42.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox42.Location = New System.Drawing.Point(651, 278)
+        Me.TextBox42.Location = New System.Drawing.Point(853, 278)
         Me.TextBox42.Multiline = True
         Me.TextBox42.Name = "TextBox42"
         Me.TextBox42.Size = New System.Drawing.Size(202, 39)
@@ -466,6 +489,7 @@ Partial Class Form2
         '
         'TextBox43
         '
+        Me.TextBox43.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.TextBox43.BackColor = System.Drawing.SystemColors.Info
         Me.TextBox43.Font = New System.Drawing.Font("Verdana", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.TextBox43.Location = New System.Drawing.Point(44, 384)
@@ -478,6 +502,7 @@ Partial Class Form2
         '
         'TextBox44
         '
+        Me.TextBox44.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.TextBox44.BackColor = System.Drawing.SystemColors.Info
         Me.TextBox44.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.TextBox44.Location = New System.Drawing.Point(253, 384)
@@ -487,12 +512,23 @@ Partial Class Form2
         Me.TextBox44.TabIndex = 43
         Me.TextBox44.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
+        'PictureBox1
+        '
+        Me.PictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.PictureBox1.Image = Global.WindowsApplication3.My.Resources.Resources.KENDO1
+        Me.PictureBox1.Location = New System.Drawing.Point(456, 31)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(180, 259)
+        Me.PictureBox1.TabIndex = 44
+        Me.PictureBox1.TabStop = False
+        '
         'Form2
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.Menu
-        Me.ClientSize = New System.Drawing.Size(915, 463)
+        Me.ClientSize = New System.Drawing.Size(1067, 730)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.TextBox44)
         Me.Controls.Add(Me.TextBox43)
         Me.Controls.Add(Me.TextBox42)
@@ -539,7 +575,8 @@ Partial Class Form2
         Me.Controls.Add(Me.TextBox1)
         Me.ForeColor = System.Drawing.SystemColors.WindowText
         Me.Name = "Form2"
-        Me.Text = "Form2"
+        Me.Text = "Scoreboard"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -589,4 +626,5 @@ Partial Class Form2
     Friend WithEvents TextBox42 As TextBox
     Friend WithEvents TextBox43 As TextBox
     Friend WithEvents TextBox44 As TextBox
+    Friend WithEvents PictureBox1 As PictureBox
 End Class
