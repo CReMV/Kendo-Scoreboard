ac023773d5bef466095d49f5ebce10b14a07a4c2
CReMV <chrisvasilas1993@yahoo.gr>
2016-11-30 17:57:32 +0200

    Added Icons to Forms

--- WindowsApplication3/Form2.Designer.vb
+++ WindowsApplication3/Form2.Designer.vb
@@ -22,6 +22,7 @@ Partial Class Form2
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form2))
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.TextBox3 = New System.Windows.Forms.TextBox()
@@ -690,6 +691,7 @@ Partial Class Form2
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.TextBox1)
         Me.ForeColor = System.Drawing.SystemColors.WindowText
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Form2"
         Me.Text = "Form2"
         Me.ResumeLayout(False)
